e18f2ada1bc6e52b338b67930007f6aabf0f7281
maitiSoutrik <soutrikmaiti.uta@gmail.com>
2022-10-16 20:14:12 -0400

    Solution fixing

--- simple_thread_assignment1/pthread.c
+++ simple_thread_assignment1/pthread.c
@@ -6,17 +6,15 @@
 
 typedef struct
 {
-    int threadIdx;
+   int threadIdx;
 } threadParams_t;
 
-// POSIX thread declarations and scheduling attributes
-//
 pthread_t threads[1];
 threadParams_t threadParams[1];
 
-void *pthread(void *threadp)
+void *pthread(void *threadp) 
 {
-    syslog(LOG_INFO, "[COURSE:1][ASSIGNMENT:1] Hello World from Thread!\n");
+   syslog(LOG_INFO, "[COURSE:5623][ASSIGNMENT:1] Hello World from Thread!\n");
 }
 
 int main (int argc, char *argv[])
@@ -24,22 +22,16 @@ int main (int argc, char *argv[])
    int rc;
    int i = 0;
 
-   syslog(LOG_INFO, "[COURSE:1][ASSIGNMENT:1] Hello World from Main!\n");
+   syslog(LOG_INFO, "[COURSE:5623][ASSIGNMENT:1] Hello World from Main!\n");
 
-   threadParams[i].threadIdx=i;
+   threadParams[i].threadIdx = i;
+   pthread_create(&threads[i],
+		  (void *)0, 
+		  pthread, 
+		  (void *)&(threadParams[i])
+		 );
 
-    pthread_create(&threads[i],   // pointer to thread descriptor
-                    (void *)0,     // use default attributes
-                    pthread, // thread function entry point
-                    (void *)&(threadParams[i]) // parameters to pass in
-                    );
-
-    pthread_join(threads[i], NULL);
-
-    closelog();
-
-    printf("TEST COMPLETE\n");
-
-    return 0;
+   pthread_join(threads[i], NULL);
+   
+   closelog();
 }
-
